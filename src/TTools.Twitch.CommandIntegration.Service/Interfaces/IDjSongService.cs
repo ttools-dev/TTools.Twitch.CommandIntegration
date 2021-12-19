8bd727d546093501d7c075657e596c15e6a26fee
James Upjohn <jammehcow@jammehcow.co.nz>
2021-12-19 21:39:38 +1300

    feat(pubby): create DJ service interface

---
+++ src/TTools.Twitch.CommandIntegration.Service/Interfaces/IDjSongService.cs
@@ -0,0 +1,16 @@
+﻿using TTools.Twitch.CommandIntegration.Service.Models;
+
+namespace TTools.Twitch.CommandIntegration.Service.Interfaces;
+
+/// <summary>
+/// Actions around DJ-like services (pubby, plug.dj etc.)
+/// </summary>
+public interface IDjSongService
+{
+    /// <summary>
+    /// Gets the currently playing song for the given room ID
+    /// </summary>
+    /// <param name="roomId">The DJ's room ID (path segment)</param>
+    /// <returns>The current song</returns>
+    Task<CurrentSong> GetCurrentSongForRoomAsync(string roomId);
+}
