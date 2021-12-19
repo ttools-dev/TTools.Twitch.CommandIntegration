e83e7741732566d1d7f1b1e2b81c3ac45d5953d0
James Upjohn <jammehcow@jammehcow.co.nz>
2021-12-19 21:49:40 +1300

    feat(ioc): register DJ service with implementation

--- src/TTools.Twitch.CommandIntegration.Api/Program.cs
+++ src/TTools.Twitch.CommandIntegration.Api/Program.cs
@@ -1,3 +1,6 @@
+using TTools.Twitch.CommandIntegration.Service.Interfaces;
+using TTools.Twitch.CommandIntegration.Service.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddScoped<IDjSongService, PubbySongService>();
 
 var app = builder.Build();
 
