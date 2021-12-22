8ff2675a35d18f420aed4fa03f3c88541d684755
James Upjohn <jammehcow@jammehcow.co.nz>
2021-12-22 17:11:21 +1300

    feat(pubby): add temporary placeholder data for room lookups

--- src/TTools.Twitch.CommandIntegration.Service/Services/PubbySongService.cs
+++ src/TTools.Twitch.CommandIntegration.Service/Services/PubbySongService.cs
@@ -8,6 +8,12 @@ public class PubbySongService : IDjSongService
 {
     public async Task<OneOf<CurrentSong, NotPlaying, NotFound, Unavailable>> GetCurrentSongForRoomAsync(string roomId)
     {
-        return CurrentSong.CreateInstance("No title", "requester", DateTime.Now).Result;
+        return roomId switch
+        {
+            "found" => CurrentSong.CreateInstance("Darude Sandstorm", "jammehcow", "", DateTime.Now).Result,
+            "no-song" => NotPlaying.CreateInstance(DateTime.Now).Result,
+            "unavailable" => Unavailable.CreateInstance("Service timed out", DateTime.Now).Result,
+            _ => NotFound.CreateInstance(DateTime.Now).Result
+        };
     }
 }
